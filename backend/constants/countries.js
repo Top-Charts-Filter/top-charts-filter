8250618f33c76e269e43e1d90f07ab71965031e5
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-07 14:53:30 +0300

    create countries constant

---
+++ backend/constants/countries.js
@@ -0,0 +1,22 @@
+/* this file contains all the countries in the system */
+
+const countries = {
+    unitedStates: {
+        name: "United States",
+        code: "US"
+    },
+    germany: {
+        name: "Germany",
+        code: "DE"
+    },
+    unitedKingdom: {
+        name: "United Kingdom",
+        code: "GB"
+    },
+    australia:{
+        name: "Australia",
+        code: "AU"
+    }
+};
+
+export default countries;
