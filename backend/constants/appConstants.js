527e825b2b38a5fa839f8dba55e3e4abb9a45bd9
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-07 15:02:04 +0300

    [bug fix] export the constants about app entity

--- backend/constants/appConstants.js
+++ backend/constants/appConstants.js
@@ -2,7 +2,7 @@
 
 /* This enumeration stores the types of operating systems where an application
     is run */
-const OperatingSystems = {
+export const OperatingSystems = {
     IOS: "ios",
     ANDROID: "android"
 }
@@ -10,7 +10,7 @@ const OperatingSystems = {
 /* This enumeration stores the types that an application has
     about its earnings method
     it can be free, paid or a top-grossing app */
-const AppEarningsTypes = {
+export const AppEarningsTypes = {
     FREE: "free",
     PAID: "paid",
     TOP_GROSSING: "top-grossing"
@@ -18,13 +18,15 @@ const AppEarningsTypes = {
 
 /* This enumeration stores the types of the devices that an application 
     is developed for */
-const DeviceTypes = {
+export const DeviceTypes = {
     ANDROID_MOBILE: "android",
     IPHONE: "iphone",
     IPAD: "ipad"
 }
 
 /* This enumeration stores the types of an application */
-const Categories = {
+export const Categories = {
     GAMES: "games"
 }
+
+
