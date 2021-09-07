2653117790f43936f74dab6aaff788387acb6462
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-07 14:24:35 +0300

    operating systems enumeration

---
+++ backend/constants/appConstants.js
@@ -0,0 +1,9 @@
+/* Constants About the 'App' Entity */
+
+/* This enumeration stores the types of operating systems where an application
+    is run */
+const OperatingSystems = {
+    IOS: "ios",
+    ANDROID: "android"
+}
+
