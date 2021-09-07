9feac9343261beeaa0c04fa35d156f37d3d5ec5f
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-07 17:16:53 +0300

    created global constants that are available around the application

---
+++ backend/constants/globalConstants.js
@@ -0,0 +1,8 @@
+/* this file constains the application level (global) constanst 
+    that will be used everywhere in the application */
+
+export const SENSORTOWER_BASE_URL = "https://sensortower.com";
+
+
+/* when the application fails, this flag indicates it */
+export const FAILURE = 1;
