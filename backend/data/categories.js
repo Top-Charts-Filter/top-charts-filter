22866ebf5b5999b64237e6243cf13072d28b31a8
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-15 21:47:16 +0300

    create categories for ios

---
+++ backend/data/categories.js
@@ -0,0 +1,404 @@
+/* This file contains all the categories in the system */
+import { OperatingSystems } from "../constants/appConstants.js";
+
+const categories = [
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Books",
+        categoryId: "6018",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Business",
+        categoryId: "6000",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Developer Tools",
+        categoryId: "6026",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Education",
+        categoryId: "6017",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Entertainment",
+        categoryId: "6016",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Finance",
+        categoryId: "6015",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Food & Drink",
+        categoryId: "6023",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Games",
+        categoryId: "6014",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Action",
+        categoryId: "7001",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Adventure",
+        categoryId: "7002",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Board",
+        categoryId: "7004",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Card",
+        categoryId: "7005",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Casino",
+        categoryId: "7006",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Casual",
+        categoryId: "7003",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Family",
+        categoryId: "7009",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Music",
+        categoryId: "7011",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Puzzle",
+        categoryId: "7012",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Racing",
+        categoryId: "7013",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Role Playing",
+        categoryId: "7014",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Simulation",
+        categoryId: "7015",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Sports",
+        categoryId: "7016",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Strategy",
+        categoryId: "7017",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Trivia",
+        categoryId: "7018",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Word",
+        categoryId: "7019",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Hyper-Casual",
+        categoryId: "9099",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: false
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Graphics & Design",
+        categoryId: "6027",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Health & Fitness",
+        categoryId: "6013",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Kids",
+        categoryId: "9007",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Ages 5 & Under",
+        categoryId: "10000",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Ages 6-8",
+        categoryId: "10001",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Ages 9-11",
+        categoryId: "10002",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Lifestyle",
+        categoryId: "6012",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Medical",
+        categoryId: "6020",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Music",
+        categoryId: "6011",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Navigation",
+        categoryId: "6010",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "News",
+        categoryId: "6009",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Photo & Video",
+        categoryId: "6008",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Productivity",
+        categoryId: "6007",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Reference",
+        categoryId: "6006",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Social Networking",
+        categoryId: "6005",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Shopping",
+        categoryId: "6024",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Sports",
+        categoryId: "6004",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Travel",
+        categoryId: "6003",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    },
+    {
+        os: OperatingSystems.IOS,
+        categoryName: "Utilities",
+        categoryId: "6002",
+        apps: [],
+        subCategories: [],
+        parent: null,
+        fetchable: true
+    }
+];
+
+
+export default categories;
