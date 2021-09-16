da0362f55cbef7ec23946a79ebc1bbd1b3e52934
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-16 13:48:34 +0300

    [data] added top 11 countries to the system

--- backend/constants/countries.js
+++ backend/constants/countries.js
@@ -1,22 +1,51 @@
-/* this file contains all the countries in the system */
+/* this file contains all the countries and a validation method for countries in the system */
 
-const Countries = {
-    UnitedStates: {
+export const Countries = {
+    "UnitedStates": {
         name: "United States",
         code: "US"
     },
-    Germany: {
+    "Australia":{
+        name: "Australia",
+        code: "AU"
+    },
+    "Canada":{
+        name: "Canada",
+        code: "CA"
+    },
+    "China":{
+        name: "China",
+        code: "CN"
+    },
+    "France":{
+        name: "France",
+        code: "FR"
+    },
+    "Germany": {
         name: "Germany",
         code: "DE"
     },
-    UnitedKingdom: {
-        name: "United Kingdom",
+    "GreatBritain": {
+        name: "Great Britain",
         code: "GB"
     },
-    Australia:{
-        name: "Australia",
-        code: "AU"
-    }
+    "Italy": {
+        name: "Italy",
+        code: "IT"
+    },
+    "SouthKorea": {
+        name: "South Korea",
+        code: "KR"
+    },
+    "Japan": {
+        name: "Japan",
+        code: "JP"
+    },
+    "Russia": {
+        name: "Russia",
+        code: "RU"
+    },
+
+    
 };
 
-export default Countries;
