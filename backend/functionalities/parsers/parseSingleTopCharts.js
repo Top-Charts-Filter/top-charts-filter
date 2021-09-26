081bdd626367cf80b9fc5becae8b38254f1e73b1
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-26 15:44:35 +0300

    add to-dos to parse single top charts

--- backend/functionalities/parsers/parseSingleTopCharts.js
+++ backend/functionalities/parsers/parseSingleTopCharts.js
@@ -5,6 +5,12 @@ async function parseSingleTopCharts(){
        it means it creates a database from a given top charts
        also puts applications rankings into the database
        it parses top charts */
+
+
+    // To-do: convert dates into real dates
+    // To-do: Remove rank
+    // To-do: Call stamper for ranking
+
 }
 
 export default parseSingleTopCharts;
