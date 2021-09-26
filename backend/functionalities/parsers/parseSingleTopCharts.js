048e59a16415e4f35387a1e230a3f1bff0bd7bb8
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-26 15:26:17 +0300

    create parser, parse single top charts (not implemented)

---
+++ backend/functionalities/parsers/parseSingleTopCharts.js
@@ -0,0 +1,10 @@
+
+
+async function parseSingleTopCharts(){
+    /* this function will parse a single top charts
+       it means it creates a database from a given top charts
+       also puts applications rankings into the database
+       it parses top charts */
+}
+
+export default parseSingleTopCharts;
