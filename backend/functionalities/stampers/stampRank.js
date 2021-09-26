5ac56cc8c070458774561ae4c9eb5192c492a1ed
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-26 15:26:39 +0300

    create a stamper, stamp rank of an app (not implemented)

---
+++ backend/functionalities/stampers/stampRank.js
@@ -0,0 +1,10 @@
+
+
+async function stampRank(){
+    /* this function puts a rank stamp into the application in the database
+       in order words, it adds information about the applications ranking 
+       on a specific country, date and category */
+    
+}
+
+export default stampRank;
